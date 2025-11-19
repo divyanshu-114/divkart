70378e658d6b73db65bb7a0295514a5cbe92216b
Divyanshu Raj <divyanshu.singh.1879@gmail.com>
2025-11-19 12:10:58 +0530

    First commit

---
+++ server/database/db.js
@@ -0,0 +1,18 @@
+import pkg from "pg";
+const { Client } = pkg;
+
+const database = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+database.connect()
+  .then(() => console.log("Connected to database successfully"))
+  .catch(err => {
+    console.error("Connection error:", err);
+    process.exit(1);
+  });
+
+export default database;
