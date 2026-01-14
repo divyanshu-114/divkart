c958dc886ef05c7615421a382508e5b70c3b0bbc
Divyanshu Raj <divyanshu.singh.1879@gmail.com>
2026-01-15 01:20:45 +0530

    admin api added

--- server/router/adminRoutes.js
+++ server/router/adminRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getAllUsers } from "../controllers/adminController";
+import { getAllUsers ,deleteUser , dashboardStats} from "../controllers/adminController";
 import { authorizedRoles, isAuthenticated } from "../middlewares/authMiddleware";
 const router = express.Router();
 
-router.get("/getallusers", isAuthenticated, authorizedRoles("admin"), getAllUsers); // dashboard
+router.get("/getallusers", isAuthenticated, authorizedRoles("Admin"), getAllUsers); // dashboard
+router.delete("/delete/:id", isAuthenticated, authorizedRoles("Admin"), deleteUser);
+router.get("/fetch/dashboard-stats", isAuthenticated, authorizedRoles("Admin"), dashboardStats);
 
 export default router; 
 
