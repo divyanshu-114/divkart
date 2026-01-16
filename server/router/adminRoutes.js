4688d20a80def002a00b33c16d910b41f481cd2c
Divyanshu Raj <divyanshu.singh.1879@gmail.com>
2026-01-16 14:52:12 +0530

    admim api created

--- server/router/adminRoutes.js
+++ server/router/adminRoutes.js
@@ -1,11 +1,29 @@
 import express from "express";
-import { getAllUsers ,deleteUser , dashboardStats} from "../controllers/adminController";
-import { authorizedRoles, isAuthenticated } from "../middlewares/authMiddleware";
+import { getAllUsers, deleteUser, dashboardStats } from "../controllers/adminController.js";
+import {
+  authorizedRoles,
+  isAuthenticated,
+} from "../middlewares/authMiddleware.js";
+
 const router = express.Router();
 
-router.get("/getallusers", isAuthenticated, authorizedRoles("Admin"), getAllUsers); // dashboard
-router.delete("/delete/:id", isAuthenticated, authorizedRoles("Admin"), deleteUser);
-router.get("/fetch/dashboard-stats", isAuthenticated, authorizedRoles("Admin"), dashboardStats);
+router.get(
+  "/getallusers",
+  isAuthenticated,
+  authorizedRoles("Admin"),
+  getAllUsers
+); // DASHBOARD
+router.delete(
+  "/delete/:id",
+  isAuthenticated,
+  authorizedRoles("Admin"),
+  deleteUser
+);
+router.get(
+  "/fetch/dashboard-stats",
+  isAuthenticated,
+  authorizedRoles("Admin"),
+  dashboardStats
+);
 
-export default router; 
-
+export default router;
