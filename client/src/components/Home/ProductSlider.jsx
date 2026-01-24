2fac56564c47134932250c0e735d79e901de3cff
Divyanshu Raj <divyanshu.singh.1879@gmail.com>
2026-01-24 18:04:01 +0530

    product slide bar created

--- client/src/components/Home/ProductSlider.jsx
+++ client/src/components/Home/ProductSlider.jsx
@@ -21,7 +21,8 @@ const ProductSlider = ({ title, products }) => {
   const handleAddToCart = (product , e)=>{
     e.preventDefault();
     e.stopPropagation();
-    dispatch(addToCart(product));
+    console.log("jgvjgv", product)
+    dispatch(addToCart({product, quantity:1 }));
     // dispatch(addToCart(product));
   }
   return <>
@@ -81,6 +82,7 @@ const ProductSlider = ({ title, products }) => {
                 <div className="flex items-center space-x-2 mb-2">
                   <div className="flex items-center">
                     {
+    
                       [...Array(5)].map((_, i) => {
                         return <Star key={i} className={`w-4 h-4 ${i < Math.floor(product.ratings) ? 'text-yellow-400 fill-current' : 'text-gray-300'}`}/>
                       })
